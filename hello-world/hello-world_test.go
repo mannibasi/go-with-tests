dbbe38a75cc52d7d78ec32eae0d28c6c46b08e9c
= <manni.basi@gmail.com>
2024-10-28 05:47:12 +0000

    Refactored test / introduced use of subtests

--- hello-world/hello-world_test.go
+++ hello-world/hello-world_test.go
@@ -2,20 +2,22 @@ package hello
 
 import "testing"
 
-func TestHelloWorld(t *testing.T) {
-	got := HelloWorld("")
-	want := "Hello, world"
+func TestHello(t *testing.T) {
+	t.Run("Say 'Hello, world' when an empty string is supplied", func(t *testing.T) {
+		got := HelloWorld("")
+		want := "Hello, world"
 
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
-	}
-}
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
+	t.Run("Say more personalised 'Hello, <name>' when a name is provided", func(t *testing.T) {
+		got := HelloWorld("Manni")
+		want := "Hello, Manni"
 
-func TestHelloPersonalised(t *testing.T) {
-	got := HelloWorld("Manni")
-	want := "Hello, Manni"
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
 
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
-	}
 }

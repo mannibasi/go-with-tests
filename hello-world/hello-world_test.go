69db069c4fa01e1384b0c6bf79a6678eb9a95de8
= <manni.basi@gmail.com>
2024-10-28 05:36:47 +0000

    Added failing/non-compiling test for expected behaviour

--- hello-world/hello-world_test.go
+++ hello-world/hello-world_test.go
@@ -10,3 +10,12 @@ func TestHelloWorld(t *testing.T) {
 		t.Errorf("got %q want %q", got, want)
 	}
 }
+
+func TestHelloPersonalised(t *testing.T) {
+	got := HelloWorld("Manni")
+	want := "Hello, Manni"
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}

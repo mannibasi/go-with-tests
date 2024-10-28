3b080289999a37d26f9b41e72c40ba62a54e292f
= <manni.basi@gmail.com>
2024-10-28 16:33:08 +0000

    Minor refactor, introducing constant for English prefix

--- hello-world/hello-world.go
+++ hello-world/hello-world.go
@@ -1,8 +1,10 @@
 package hello
 
+const englishHelloPrefix = "Hello, "
+
 func HelloWorld(name string) string {
 	if name == "" {
 		name = "world"
 	}
-	return "Hello, " + name
+	return englishHelloPrefix + name
 }
--- hello-world/hello-world_test.go
+++ hello-world/hello-world_test.go
@@ -6,18 +6,19 @@ func TestHello(t *testing.T) {
 	t.Run("Say 'Hello, world' when an empty string is supplied", func(t *testing.T) {
 		got := HelloWorld("")
 		want := "Hello, world"
-
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
 	t.Run("Say more personalised 'Hello, <name>' when a name is provided", func(t *testing.T) {
 		got := HelloWorld("Manni")
 		want := "Hello, Manni"
+		assertCorrectMessage(t, got, want)
 
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
 	})
+}
 
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
 }

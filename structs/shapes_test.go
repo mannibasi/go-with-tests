337d437ce255b1371cbfd39d18e59be2822564d9
= <manni.basi@gmail.com>
2024-11-18 20:55:12 +0000

    Refactored to wrap each test case in its own t.Run()

--- structs/shapes_test.go
+++ structs/shapes_test.go
@@ -14,18 +14,21 @@ func TestPerimeter(t *testing.T) {
 
 func TestArea(t *testing.T) {
 	areaTests := []struct {
+		name    string
 		shape   Shape
 		hasArea float64
 	}{
-		{shape: Rectangle{Width: 12.0, Height: 6.0}, hasArea: 72.0},
-		{shape: Circle{Radius: 10.0}, hasArea: 314.1592653589793},
-		{shape: Triangle{Base: 12.0, Height: 6.0}, hasArea: 36.0},
+		{name: "Rectangle", shape: Rectangle{Width: 12.0, Height: 6.0}, hasArea: 72.0},
+		{name: "Circle", shape: Circle{Radius: 10.0}, hasArea: 314.1592653589793},
+		{name: "Triangle", shape: Triangle{Base: 12.0, Height: 6.0}, hasArea: 36.0},
 	}
 
 	for _, tt := range areaTests {
-		got := tt.shape.Area()
-		if got != tt.hasArea {
-			t.Errorf("%#v got %g want %g", tt.shape, got, tt.hasArea)
-		}
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
+			if got != tt.hasArea {
+				t.Errorf("%#v got %g want %g", tt.shape, got, tt.hasArea)
+			}
+		})
 	}
 }

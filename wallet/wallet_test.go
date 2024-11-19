5d45eb3a3bfc2f23a1bbd71e0ea1265fe73164c6
Manni Basi <manni.basi@gmail.com>
2024-11-19 22:01:07 +0000

    Enhanced to check for no error on successful withdrawal

--- wallet/wallet_test.go
+++ wallet/wallet_test.go
@@ -11,9 +11,10 @@ func TestWallet(t *testing.T) {
 		assertBalance(t, wallet, Bitcoin(10))
 	})
 
-	t.Run("withdraw", func(t *testing.T) {
+	t.Run("withdraw with funds", func(t *testing.T) {
 		wallet := Wallet{balance: Bitcoin(10)}
-		wallet.Withdraw(Bitcoin(3))
+		err := wallet.Withdraw(Bitcoin(13)) //TODO: temporarily forced failure to review "no error" checking
+		assertNoError(t, err)
 		assertBalance(t, wallet, Bitcoin(7))
 	})
 
@@ -34,6 +35,13 @@ func assertBalance(t testing.TB, wallet Wallet, want Bitcoin) {
 
 	if got != want {
 		t.Errorf("got %s, want %s", got, want)
+	}
+}
+
+func assertNoError(t *testing.T, err error) {
+	t.Helper()
+	if err != nil {
+		t.Fatal("got an error but didn't expect one")
 	}
 }
 

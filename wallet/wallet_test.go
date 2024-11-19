1c1148bac3f6ed065e0c86145976b6bb126bbf43
Manni Basi <manni.basi@gmail.com>
2024-11-19 21:56:46 +0000

    Refactored tests / helper functions for readability

--- wallet/wallet_test.go
+++ wallet/wallet_test.go
@@ -5,26 +5,6 @@ import (
 )
 
 func TestWallet(t *testing.T) {
-	assertError := func(t testing.TB, got error, want error) {
-		t.Helper()
-		if got == nil {
-			t.Fatal("wanted an error but didn't get one")
-		}
-
-		if got != want {
-			t.Errorf("got %q, want %q", got, want)
-		}
-	}
-
-	assertBalance := func(t testing.TB, wallet Wallet, want Bitcoin) {
-		t.Helper()
-		got := wallet.Balance()
-
-		if got != want {
-			t.Errorf("got %s, want %s", got, want)
-		}
-	}
-
 	t.Run("deposit", func(t *testing.T) {
 		wallet := Wallet{}
 		wallet.Deposit(Bitcoin(10))
@@ -46,4 +26,24 @@ func TestWallet(t *testing.T) {
 		assertBalance(t, wallet, startingBalance)
 	})
 
+}
+
+func assertBalance(t testing.TB, wallet Wallet, want Bitcoin) {
+	t.Helper()
+	got := wallet.Balance()
+
+	if got != want {
+		t.Errorf("got %s, want %s", got, want)
+	}
+}
+
+func assertError(t testing.TB, got error, want error) {
+	t.Helper()
+	if got == nil {
+		t.Fatal("wanted an error but didn't get one")
+	}
+
+	if got != want {
+		t.Errorf("got %q, want %q", got, want)
+	}
 }
